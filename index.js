c49dfeec05355d81e5ad6d407db822492fd2929c
Harry-xdev <harry.xdev@gmail.com>
2022-01-11 08:51:22 +0700

    hw4

--- index.js
+++ index.js
@@ -1,53 +1,21 @@
-let u = document.getElementById("user").value;
-let p1 = document.getElementById("password").value;
-let sub = document.getElementById("submit");
-
-
-if(u== "") {
-alert("Vui lòng nhập tên!");
-return false;
+let id = String(prompt("User Account:"));
+if (id == "Admin") {
+    let pw = String(prompt("User password:"));
+    if (pw == "cafedev") {
+        alert(`Welcome!`);
+    }
+    else if (pw == "") {
+        alert(`Canceled`);
+    }
+    else if (pw !== "cafedev") {
+        alert(`Wrong password!`);
+    }
 }
-if(p1 == "") {
-alert("Vui lòng nhập mật khẩu!");
-return false;
+else if (id == "") {
+    alert(`Canceled`);
 }
-if(p2 == "") {
-alert("Vui lòng xác minh mật khẩu!");
-return false;
+else if (id !== "Admin") {
+    alert(`I don't know you!`);
 }
-    
-alert("Xin hãy điền đúng thông tin!")
-    
-return true;
-
-
-
-
-
-
-
-
-
-
-// let id = String(prompt("User Account:"));
-// let pw = String(prompt("User password:"));
-// function login(id, pw) {
-//     if (id == "Admin") {
-//         if (pw == "cafedev") {
-//             alert(`Welcome to the world!`)
-//         }
-//         else if (pw !== "cafedev");
-//             alert(`Wrong password!!!`);
-//         }
-//         else if (pw = null ) {
-//             alert(`ĐÃ HỦY`);
-//         }
-//         else if (id == "" || "Ecs") {
-//             alert(`Canceled`);
-//         }
-//         else if (id !== "Admin", "", "Ecs") {
-//             alert(`I don't know you`);
-//         }    
-//     }
 
 
